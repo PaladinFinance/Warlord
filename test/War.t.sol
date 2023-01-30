cc83af678edc159d32046733e0694e15b317aa75
Alberto Centonze <albicento.ac@gmail.com>
2023-01-30 09:51:47 +0100

    Cleaning access control tests

--- test/War.t.sol
+++ test/War.t.sol
@@ -6,7 +6,8 @@ import "../src/War.sol";
 import "forge-std/Test.sol";
 
 contract WarTokenTest is Test {
-  address admin = makeAddr("admin"); // admin
+  address admin = makeAddr("admin");
+  address minter = makeAddr("minter");
   address alice = makeAddr("alice");
   address bob = makeAddr("bob");
   WarToken war;
@@ -24,7 +25,7 @@ contract WarTokenTest is Test {
   }
 
   function testAdminGating() public {
-    // TODO cover more cases
+    // TODO should I test for more than 1 admin
     vm.prank(alice);
     vm.expectRevert(
       "AccessControl: account 0x328809bc894f92807417d2dad6b7c998c1afdac6 is missing role 0x0000000000000000000000000000000000000000000000000000000000000000"
@@ -33,15 +34,23 @@ contract WarTokenTest is Test {
   }
 
   function testMinterGating() public {
-    // TODO cover more cases
     vm.prank(alice);
     vm.expectRevert(
       "AccessControl: account 0x328809bc894f92807417d2dad6b7c998c1afdac6 is missing role 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6"
     );
     war.mint(bob, 100);
-  }
 
-  function testGrantMinterRole() public {
     vm.prank(admin);
+    war.grantMinterRole(alice);
+    vm.prank(alice);
+    war.mint(bob, 100);
+
+    vm.prank(admin);
+    war.revokeMinterRole(alice);
+    vm.prank(alice);
+    vm.expectRevert(
+      "AccessControl: account 0x328809bc894f92807417d2dad6b7c998c1afdac6 is missing role 0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6"
+    );
+    war.mint(bob, 100);
   }
 }
