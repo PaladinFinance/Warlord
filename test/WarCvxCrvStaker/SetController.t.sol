ecfafd6591791e255cba7e6ff01746989581e2fa
Alberto Centonze <albicento.ac@gmail.com>
2023-02-07 18:14:20 +0100

    test: Making WarCvxCrvStaker testing more robust

---
+++ test/WarCvxCrvStaker/SetController.t.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.16;
+
+import "./WarCvxCrvStakerTest.sol";
+
+contract SetController is WarCvxCrvStakerTest {
+  function testDefaultBehavior(address newController) public {
+    vm.assume(newController != zero);
+    vm.prank(admin);
+    warCvxCrvStaker.setController(newController);
+    assertEq(warCvxCrvStaker.controller(), newController);
+  }
+
+  function testRevertWithZeroAddress() public {
+    vm.expectRevert(Errors.ZeroAddress.selector);
+    vm.prank(admin);
+    warCvxCrvStaker.setController(zero);
+  }
+
+  function testOnlyOwner() public {
+    vm.expectRevert("Ownable: caller is not the owner");
+    warCvxCrvStaker.setController(alice);
+  }
+
+  function testSameValue() public {
+    vm.expectRevert(Errors.AlreadySet.selector);
+    vm.prank(admin);
+    warCvxCrvStaker.setController(controller);
+  }
+}
