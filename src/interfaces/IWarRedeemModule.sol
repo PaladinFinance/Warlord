677c42aa0b25cd486bb8d770a1dc3f09c4c5d54d
Alberto Centonze <albicento.ac@gmail.com>
2023-02-08 21:17:30 +0100

    chore: Renamed IWarRedeem interface to IWarRedeemModule

--- src/interfaces/IWarRedeemModule.sol
+++ src/interfaces/IWarRedeemModule.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity 0.8.16;
 
-interface IWarRedeem {
+interface IWarRedeemModule {
   function queuedForWithdrawal() external returns (uint256);
   function notifyUnlock(address token, uint256 amount) external;
 }
