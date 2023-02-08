3943a053bb510a4fbd802db3a111511eb3b23142
Alberto Centonze <albicento.ac@gmail.com>
2023-02-08 21:18:14 +0100

    test: Added MockRedeemModule implementing IWarRedeemModule

---
+++ test/mocks/MockRedeemModule.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.16;
+
+import "interfaces/IWarRedeemModule.sol";
+import "utils/Owner.sol";
+
+contract MockRedeem is IWarRedeemModule, Owner {
+  uint256 queued;
+
+  function queuedForWithdrawal() external view returns (uint256) {
+    return queued;
+  }
+
+  function notifyUnlock(address token, uint256 amount) external onlyOwner {
+    if (token != address(0)) {
+      queued -= amount;
+    }
+  }
+
+  // Mock function for testing purposes
+  function setQueue(uint256 amount) external {
+    queued = amount;
+  }
+}
