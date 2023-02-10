ee409e7d8d10581156c20f4b7f54ad2adcc81ab0
Alberto Centonze <albicento.ac@gmail.com>
2023-02-10 10:36:29 +0100

    feat: Added functions to IFarmer interface

--- src/interfaces/IFarmer.sol
+++ src/interfaces/IFarmer.sol
@@ -6,6 +6,7 @@ import {IHarvestable} from "./IHarvestable.sol";
 interface IFarmer is IHarvestable {
   // the index stored by the farmer represents all the recevied tokens
   function getCurrentIndex() external view returns (uint256);
-
   function sendTokens(address receiver, uint256 amount) external;
+  function harvest() external;
+  function stake(address token, uint256 amount) external;
 }
