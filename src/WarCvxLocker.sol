1f62d3ab629038b03fc95abf5d286efb5b872d31
Alberto Centonze <albicento.ac@gmail.com>
2023-02-10 19:53:44 +0100

    feat: Implementing WarCvxLocker

--- src/WarCvxLocker.sol
+++ src/WarCvxLocker.sol
@@ -12,22 +12,34 @@ import {Owner} from "utils/Owner.sol";
 import {Errors} from "utils/Errors.sol";
 import {IWarRedeemModule} from "interfaces/IWarRedeemModule.sol";
 import {Math} from "openzeppelin/utils/math/Math.sol";
+import {WarMinter} from "src/WarMinter.sol";
 
 contract WarCvxLocker is IWarLocker, Pausable, Owner, ReentrancyGuard {
   CvxLockerV2 private constant locker = CvxLockerV2(0x72a19342e8F1838460eBFCCEf09F6585e32db86E);
   IERC20 private constant cvx = IERC20(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
-  IDelegateRegistry public constant registry = IDelegateRegistry(0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446);
+  IDelegateRegistry private constant registry = IDelegateRegistry(0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446);
 
   IWarRedeemModule public redeemModule;
+  WarMinter warMinter;
   address public controller;
-  // TODO should I make a variable for the delegatee
+  address public delegatee;
+  // TODO add events
 
   using SafeERC20 for IERC20;
 
-  constructor(address _controller, address _redeemModule, address _delegatee) {
-    if (_controller == address(0) || _redeemModule == address(0)) revert Errors.ZeroAddress();
+  modifier onlyWarMinter() {
+    if (address(warMinter) != msg.sender) revert Errors.CallerNotAllowed();
+    _;
+  }
+
+  constructor(address _controller, address _redeemModule, address _warMinter, address _delegatee) {
+    if (_controller == address(0) || _redeemModule == address(0) || _warMinter == address(0)) {
+      revert Errors.ZeroAddress();
+    }
+    warMinter = WarMinter(_warMinter);
     controller = _controller;
     redeemModule = IWarRedeemModule(_redeemModule);
+    delegatee = _delegatee;
     registry.setDelegate("cvx.eth", _delegatee);
   }
 
@@ -35,7 +47,7 @@ contract WarCvxLocker is IWarLocker, Pausable, Owner, ReentrancyGuard {
     return address(cvx);
   }
 
-  function lock(uint256 amount) public {
+  function lock(uint256 amount) external onlyWarMinter whenNotPaused {
     if (amount == 0) revert Errors.ZeroValue();
 
     cvx.safeTransferFrom(msg.sender, address(this), amount);
@@ -43,12 +55,13 @@ contract WarCvxLocker is IWarLocker, Pausable, Owner, ReentrancyGuard {
     locker.lock(address(this), amount, 0); // TODO what is _spendRatio
   }
 
-  function harvest() public {
+  function harvest() public whenNotPaused {
     locker.getReward(controller);
   }
 
-  function delegate(address delegatee) external onlyOwner {
-    registry.setDelegate("cvx.eth", delegatee);
+  function setDelegate(address _delegatee) external onlyOwner {
+    delegatee = _delegatee;
+    registry.setDelegate("cvx.eth", _delegatee);
   }
 
   function processUnlock() external {
@@ -59,19 +72,31 @@ contract WarCvxLocker is IWarLocker, Pausable, Owner, ReentrancyGuard {
 
     uint256 withdrawalAmount = redeemModule.queuedForWithdrawal();
 
-    // If unlock == 0 relock, otherwise withdraw everything and lock only what's left
+    // If unlock == 0 relock everything
     if (withdrawalAmount == 0) {
       locker.processExpiredLocks(true);
       return;
     } else {
+      // otherwise withdraw everything and lock only what's left
       locker.processExpiredLocks(false);
       withdrawalAmount = Math.min(unlockableBalance, withdrawalAmount);
       cvx.transfer(address(redeemModule), withdrawalAmount);
       redeemModule.notifyUnlock(address(cvx), withdrawalAmount);
 
+      // TODO are variable assignment that expensive gas wise
       uint256 relock = unlockableBalance - withdrawalAmount;
       cvx.safeApprove(address(locker), relock);
-      lock(relock);
+      locker.lock(address(this), relock, 0);
     }
+  }
+
+  function migrate() external onlyOwner whenPaused {}
+
+  function pause() external onlyOwner {
+    _pause();
+  }
+
+  function unpause() external onlyOwner {
+    _unpause();
   }
 }
