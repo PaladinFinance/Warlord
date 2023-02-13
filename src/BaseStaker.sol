a2fc3309a2b8721fabe6c1ba1456745b47ea4147
Alberto Centonze <albicento.ac@gmail.com>
2023-02-13 15:58:49 +0100

    feat: Abstracted base logic from WarCvxCrvStaker for auraBAL

---
+++ src/BaseStaker.sol
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.16;
+
+import {Owner} from "utils/Owner.sol";
+import {Errors} from "utils/Errors.sol";
+import {Pausable} from "openzeppelin/security/Pausable.sol";
+import {IFarmer} from "interfaces/IFarmer.sol";
+import {ReentrancyGuard} from "solmate/utils/ReentrancyGuard.sol";
+
+abstract contract BaseStaker is Owner, Pausable, ReentrancyGuard {
+  address public controller;
+  address public warStaker;
+
+  uint256 internal _index;
+
+  event SetController(address controller);
+  event SetWarStaker(address warStaker);
+  event Staked(uint256 amount, uint256 index);
+
+  constructor(address _controller, address _warStaker) {
+    if (_controller == address(0) || _warStaker == address(0)) revert Errors.ZeroAddress();
+    controller = _controller;
+    warStaker = _warStaker;
+  }
+
+  modifier onlyController() {
+    if (controller != msg.sender) revert Errors.CallerNotAllowed();
+    _;
+  }
+
+  modifier onlyWarStaker() {
+    if (warStaker != msg.sender) revert Errors.CallerNotAllowed();
+    _;
+  }
+
+  function getCurrentIndex() external view returns (uint256) {
+    return _index;
+  }
+
+  function setController(address _controller) external onlyOwner {
+    if (_controller == address(0)) revert Errors.ZeroAddress();
+    if (_controller == controller) revert Errors.AlreadySet();
+    controller = _controller;
+
+    emit SetController(_controller);
+  }
+
+  function setWarStaker(address _warStaker) external onlyOwner {
+    if (_warStaker == address(0)) revert Errors.ZeroAddress();
+    if (_warStaker == warStaker) revert Errors.AlreadySet();
+    warStaker = _warStaker;
+
+    emit SetWarStaker(_warStaker);
+  }
+
+  function migrate(address receiver) external virtual;
+
+  function pause() external onlyOwner {
+    _pause();
+  }
+
+  function unpause() external onlyOwner {
+    _unpause();
+  }
+}
--- src/WarCvxCrvStaker.sol
+++ src/WarCvxCrvStaker.sol
@@ -1,73 +1,25 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity 0.8.16;
 
-import {IFarmer} from "interfaces/IFarmer.sol";
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
-import {Pausable} from "openzeppelin/security/Pausable.sol";
-import {Owner} from "utils/Owner.sol";
-import {CvxCrvStakingWrapper} from "interfaces/external/CvxCrvStakingWrapper.sol";
+import {CvxCrvStaking} from "interfaces/external/CvxCrvStaking.sol";
 import {CrvDepositor} from "interfaces/external/CrvDepositor.sol";
-import {Errors} from "utils/Errors.sol";
-import {ReentrancyGuard} from "solmate/utils/ReentrancyGuard.sol";
+import "./BaseStaker.sol";
 
-// TODO make common base class for auraBal
 // TODO test for event emission
-contract WarCvxCrvStaker is IFarmer, Owner, Pausable, ReentrancyGuard {
+contract WarCvxCrvStaker is BaseStaker {
   IERC20 private constant crv = IERC20(0xD533a949740bb3306d119CC777fa900bA034cd52);
   IERC20 private constant cvxCrv = IERC20(0x62B9c7356A2Dc64a1969e19C23e4f579F9810Aa7);
-  CvxCrvStakingWrapper private constant cvxCrvStakingWrapper =
-    CvxCrvStakingWrapper(0xaa0C3f5F7DFD688C6E646F66CD2a6B66ACdbE434);
+  CvxCrvStaking private constant cvxCrvStaking = CvxCrvStaking(0xaa0C3f5F7DFD688C6E646F66CD2a6B66ACdbE434);
   CrvDepositor private constant crvDepositor = CrvDepositor(0x8014595F2AB54cD7c604B00E9fb932176fDc86Ae);
-
-  address public controller;
-  address public warStaker;
-  uint256 private _index;
 
   using SafeERC20 for IERC20;
 
-  event SetController(address controller);
-  event SetWarStaker(address warStaker);
-  event Staked(uint256 amount, uint256 index);
-
-  constructor(address _controller, address _warStaker) {
-    if (_controller == address(0) || _warStaker == address(0)) revert Errors.ZeroAddress();
-    controller = _controller;
-    warStaker = _warStaker;
-  }
-
-  modifier onlyController() {
-    if (controller != msg.sender) revert Errors.CallerNotAllowed();
-    _;
-  }
-
-  modifier onlyWarStaker() {
-    if (warStaker != msg.sender) revert Errors.CallerNotAllowed();
-    _;
-  }
-
-  function getCurrentIndex() external view returns (uint256) {
-    return _index;
-  }
-
-  function setController(address _controller) external onlyOwner {
-    if (_controller == address(0)) revert Errors.ZeroAddress();
-    if (_controller == controller) revert Errors.AlreadySet();
-    controller = _controller;
-
-    emit SetController(_controller);
-  }
-
-  function setWarStaker(address _warStaker) external onlyOwner {
-    if (_warStaker == address(0)) revert Errors.ZeroAddress();
-    if (_warStaker == warStaker) revert Errors.AlreadySet();
-    warStaker = _warStaker;
-
-    emit SetWarStaker(_warStaker);
-  }
+  constructor(address _controller, address _warStaker) BaseStaker(_controller, _warStaker) {}
 
   function setRewardWeight(uint256 weight) external onlyOwner whenNotPaused {
-    cvxCrvStakingWrapper.setRewardWeight(weight);
+    cvxCrvStaking.setRewardWeight(weight);
   }
 
   function stake(address token, uint256 amount) external onlyController whenNotPaused nonReentrant {
@@ -87,47 +39,38 @@ contract WarCvxCrvStaker is IFarmer, Owner, Pausable, ReentrancyGuard {
     } else {
       _index += amount;
     }
-    cvxCrv.safeApprove(address(cvxCrvStakingWrapper), amount);
-    cvxCrvStakingWrapper.stake(amount, address(this));
+    cvxCrv.safeApprove(address(cvxCrvStaking), amount);
+    cvxCrvStaking.stake(amount, address(this));
 
     emit Staked(amount, _index);
   }
 
-  // TODO not sure nonReentrant is really useful for this function
   function harvest() external whenNotPaused nonReentrant {
     _harvest();
   }
 
   function _harvest() internal {
-    cvxCrvStakingWrapper.getReward(address(this), controller);
+    cvxCrvStaking.getReward(address(this), controller);
   }
 
   function sendTokens(address receiver, uint256 amount) external onlyWarStaker whenNotPaused nonReentrant {
     if (receiver == address(0)) revert Errors.ZeroAddress();
     if (amount == 0) revert Errors.ZeroValue();
-    if (cvxCrvStakingWrapper.balanceOf(address(this)) < amount) revert Errors.UnstakingMoreThanBalance();
+    if (cvxCrvStaking.balanceOf(address(this)) < amount) revert Errors.UnstakingMoreThanBalance();
 
-    cvxCrvStakingWrapper.withdraw(amount);
+    cvxCrvStaking.withdraw(amount);
     cvxCrv.safeTransfer(receiver, amount);
   }
 
-  function migrate(address receiver) external onlyOwner whenPaused {
+  function migrate(address receiver) external override onlyOwner whenPaused {
     if (receiver == address(0)) revert Errors.ZeroAddress();
 
     // Unstake and send cvxCrv
-    uint256 cvxCrvStakedBalance = cvxCrvStakingWrapper.balanceOf(address(this));
-    cvxCrvStakingWrapper.withdraw(cvxCrvStakedBalance);
+    uint256 cvxCrvStakedBalance = cvxCrvStaking.balanceOf(address(this));
+    cvxCrvStaking.withdraw(cvxCrvStakedBalance);
     cvxCrv.safeTransfer(receiver, cvxCrvStakedBalance);
 
     // Harvest and send rewards to the controller
     _harvest();
-  }
-
-  function pause() external onlyOwner {
-    _pause();
-  }
-
-  function unpause() external onlyOwner {
-    _unpause();
   }
 }
