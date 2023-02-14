e2b31657e7458473aab77965e1b6b9d5e0d25fbe
Alberto Centonze <albicento.ac@gmail.com>
2023-02-14 12:13:40 +0100

    test: Testing setDelegate for WarCvxLocker

---
+++ test/WarCvxLocker/SetDelegate.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.16;
+
+import "./WarCvxLockerTest.sol";
+
+contract SetDelegate is WarCvxLockerTest {
+  function testDefaultBehavior(address _delegatee) public {
+    console.log(locker.owner());
+    console.log(admin);
+    vm.assume(_delegatee != locker.delegatee() && _delegatee != zero);
+    vm.startPrank(admin);
+    locker.setDelegate(_delegatee);
+    vm.stopPrank();
+    assertEq(locker.delegatee(), _delegatee);
+    assertEq(registry.delegation(address(locker), "cvx.eth"), _delegatee);
+  }
+}
