3bb3a6ee291f1282b12b6b81769e3f909444fa52
Alberto Centonze <albicento.ac@gmail.com>
2023-02-15 22:30:24 +0100

    test(AuraBalFarmer): Testing setSlippage function

--- test/AuraBalFarmer/SetSlippage.t.sol
+++ test/AuraBalFarmer/SetSlippage.t.sol
@@ -3,4 +3,24 @@ pragma solidity 0.8.16;
 
 import "./AuraBalFarmerTest.sol";
 
-contract SetSlippage is AuraBalFarmerTest {}
+contract SetSlippage is AuraBalFarmerTest {
+  function testDefaultBehavior(uint256 bps) public {
+    vm.assume(bps < 500);
+    assertEq(auraBalFarmer.slippageBps(), 9950);
+    vm.prank(admin);
+    auraBalFarmer.setSlippage(bps);
+    assertEq(auraBalFarmer.slippageBps(), 10_000 - bps);
+  }
+
+  function testSlippageTooHigh(uint256 bps) public {
+    vm.assume(bps > 500);
+    vm.prank(admin);
+    vm.expectRevert(Errors.SlippageTooHigh.selector);
+    auraBalFarmer.setSlippage(bps);
+  }
+
+  function testOnlyOwner(uint256 bps) public {
+    vm.expectRevert("Ownable: caller is not the owner");
+    auraBalFarmer.setSlippage(bps);
+  }
+}
