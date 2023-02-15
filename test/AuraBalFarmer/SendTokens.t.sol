266e04bdab09a4c1f1175c36161c69405f36633c
Alberto Centonze <albicento.ac@gmail.com>
2023-02-15 19:34:16 +0100

    test(AuraBalFarmer): Testing sendTokens function

--- test/AuraBalFarmer/SendTokens.t.sol
+++ test/AuraBalFarmer/SendTokens.t.sol
@@ -4,36 +4,52 @@ pragma solidity 0.8.16;
 import "./AuraBalFarmerTest.sol";
 
 contract SendTokens is AuraBalFarmerTest {
-/*
   function setUp() public override {
-    WarAuraBalStakerTest.setUp();
+    AuraBalFarmerTest.setUp();
     vm.startPrank(controller);
-    warAuraBalStaker.stake(address(cvxCrv), cvxCrv.balanceOf(controller));
-    warAuraBalStaker.stake(address(crv), crv.balanceOf(controller));
+    auraBalFarmer.stake(address(auraBal), auraBal.balanceOf(controller));
+    auraBalFarmer.stake(address(bal), bal.balanceOf(controller));
     vm.stopPrank();
     vm.warp(block.timestamp + 100 days);
   }
 
   function testDefaultBehavior(uint256 amount) public {
-    vm.assume(amount > 0 && amount <= convexCvxCrvStaker.balanceOf(address(warAuraBalStaker)));
-    assertEq(cvxCrv.balanceOf(alice), 0);
-    assertEq(convexCvxCrvStaker.balanceOf(address(warAuraBalStaker)), 200e18);
+    // Checkpoint of iniital staked balance of auraBal
+    uint256 initialBalance = auraBalStaker.balanceOf(address(auraBalFarmer));
+
+    // Tokens amount is non-zero and smaller than iniital balance
+    vm.assume(amount > 0 && amount <= initialBalance);
+
+    // Alice doesn't have any token before the transaction
+    assertEq(auraBal.balanceOf(alice), 0);
+
     vm.prank(address(warStaker));
-    warAuraBalStaker.sendTokens(alice, amount);
-    assertEq(cvxCrv.balanceOf(alice), amount);
+    auraBalFarmer.sendTokens(alice, amount);
+
+    // make sure alice received the correct amount
+    assertEq(auraBal.balanceOf(alice), amount);
+
+    // Check if the amount unstaked is correct
+    assertEq(auraBalStaker.balanceOf(address(auraBalFarmer)), initialBalance - amount);
   }
 
   function testZeroAmount(address randomAddress) public {
+    // TODO probably inverted names in origianl test
     vm.assume(randomAddress != zero);
     vm.expectRevert(Errors.ZeroValue.selector);
     vm.prank(address(warStaker));
-    warAuraBalStaker.sendTokens(randomAddress, 0);
+    auraBalFarmer.sendTokens(randomAddress, 0);
   }
 
   function testZeroAddress(uint256 randomValue) public {
-    vm.assume(randomValue > 0 && randomValue <= convexCvxCrvStaker.balanceOf(address(warAuraBalStaker)));
+    vm.assume(randomValue > 0 && randomValue <= auraBalStaker.balanceOf(address(auraBalFarmer)));
     vm.expectRevert(Errors.ZeroAddress.selector);
     vm.prank(address(warStaker));
-    warCvxCrvFarmer.sendTokens(zero, randomValue);
-  }*/
+    auraBalFarmer.sendTokens(zero, randomValue);
+  }
+
+  function testOnlyWarStaker() public {
+    vm.expectRevert(Errors.CallerNotAllowed.selector);
+    auraBalFarmer.sendTokens(alice, 500);
+  }
 }
