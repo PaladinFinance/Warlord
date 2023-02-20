dc917287a34f335da37f5f58ab00b9dd0e62b7cf
Alberto Centonze <albicento.ac@gmail.com>
2023-02-20 23:11:46 +0100

    feat(CvxLocker): harvest and migrate

--- src/CvxLocker.sol
+++ src/CvxLocker.sol
@@ -7,7 +7,7 @@ import {CvxLockerV2} from "interfaces/external/convex/vlCvx.sol";
 import {Math} from "openzeppelin/utils/math/Math.sol";
 
 contract WarCvxLocker is WarBaseLocker {
-  CvxLockerV2 private constant locker = CvxLockerV2(0x72a19342e8F1838460eBFCCEf09F6585e32db86E);
+  CvxLockerV2 private constant vlCvx = CvxLockerV2(0x72a19342e8F1838460eBFCCEf09F6585e32db86E);
   IERC20 private constant cvx = IERC20(0x4e3FBD56CD56c3e72c1403e103b45Db9da5B9D2B);
   IDelegateRegistry private constant registry = IDelegateRegistry(0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446);
 
@@ -31,14 +31,26 @@ contract WarCvxLocker is WarBaseLocker {
 
     cvx.safeTransferFrom(msg.sender, address(this), amount);
 
-    cvx.safeApprove(address(locker), 0);
-    cvx.safeIncreaseAllowance(address(locker), amount);
+    cvx.safeApprove(address(vlCvx), 0);
+    cvx.safeIncreaseAllowance(address(vlCvx), amount);
 
-    locker.lock(address(this), amount, 0); // TODO what is _spendRatio
+    vlCvx.lock(address(this), amount, 0); // TODO what is _spendRatio
   }
 
   function _harvest() internal override {
-    locker.getReward(controller, false);
+    CvxLockerV2.EarnedData[] memory rewards = vlCvx.claimableRewards(address(this));
+    uint256 rewardsLength = rewards.length;
+
+    vlCvx.getReward(address(this), false);
+
+    for (uint256 i; i < rewardsLength;) {
+      // TODO is it cheaper with a variable assignment instead of array access twice?
+      IERC20(rewards[i].token).safeTransfer(controller, rewards[i].amount);
+
+      unchecked {
+        ++i;
+      }
+    }
   }
 
   function setDelegate(address _delegatee) external onlyOwner {
@@ -46,31 +58,30 @@ contract WarCvxLocker is WarBaseLocker {
     registry.setDelegate("cvx.eth", _delegatee);
   }
 
-  // TODO modifiers ?
-  function processUnlock() external {
+  function _processUnlock() internal override {
     _harvest();
 
-    (, uint256 unlockableBalance,,) = locker.lockedBalances(address(this));
+    (, uint256 unlockableBalance,,) = vlCvx.lockedBalances(address(this));
     if (unlockableBalance == 0) return;
 
     uint256 withdrawalAmount = IWarRedeemModule(redeemModule).queuedForWithdrawal();
 
     // If unlock == 0 relock everything
     if (withdrawalAmount == 0) {
-      locker.processExpiredLocks(true);
+      vlCvx.processExpiredLocks(true);
       return;
     } else {
       // otherwise withdraw everything and lock only what's left
-      locker.processExpiredLocks(false);
+      vlCvx.processExpiredLocks(false);
       withdrawalAmount = Math.min(unlockableBalance, withdrawalAmount);
       cvx.transfer(address(redeemModule), withdrawalAmount);
       IWarRedeemModule(redeemModule).notifyUnlock(address(cvx), withdrawalAmount);
 
       // TODO are variable assignment that expensive gas wise
       uint256 relock = unlockableBalance - withdrawalAmount;
-      cvx.safeApprove(address(locker), 0);
-      cvx.safeIncreaseAllowance(address(locker), relock);
-      locker.lock(address(this), relock, 0);
+      cvx.safeApprove(address(vlCvx), 0);
+      cvx.safeIncreaseAllowance(address(vlCvx), relock);
+      vlCvx.lock(address(this), relock, 0);
     }
   }
 
@@ -78,6 +89,10 @@ contract WarCvxLocker is WarBaseLocker {
     if (receiver == address(0)) revert Errors.ZeroAddress();
     if (!vlCvx.isShutdown() && !isShutdown) revert Errors.LockerStillAlive();
 
-    if (locker.isShutdown() || isShutdown) {}
+    // withdraws unlockable balance to receiver
+    vlCvx.withdrawExpiredLocksTo(receiver);
+
+    // withdraws rewards to controller
+    _harvest();
   }
 }
