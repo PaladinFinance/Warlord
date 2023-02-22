e8cfcf2d97058dfa7b01b4eed1a46bcfa91a03c8
Alberto Centonze <albicento.ac@gmail.com>
2023-02-22 19:23:39 +0100

    style: Formatting
    Also ignoring in dev contract

--- src/interfaces/external/IHolyPaladinToken.sol
+++ src/interfaces/external/IHolyPaladinToken.sol
@@ -2,29 +2,27 @@
 pragma solidity 0.8.16;
 
 interface IHolyPaladinToken {
+  struct UserLock {
+    // Amount of locked balance
+    uint128 amount; // safe because PAL max supply is 10M tokens
+    // Start of the Lock
+    uint48 startTimestamp;
+    // Duration of the Lock
+    uint48 duration;
+    // BlockNumber for the Lock
+    uint32 fromBlock; // because we want to search by block number
+  }
 
-    struct UserLock {
-        // Amount of locked balance
-        uint128 amount; // safe because PAL max supply is 10M tokens
-        // Start of the Lock
-        uint48 startTimestamp;
-        // Duration of the Lock
-        uint48 duration;
-        // BlockNumber for the Lock
-        uint32 fromBlock; // because we want to search by block number
-    }
+  struct TotalLock {
+    // Total locked Supply
+    uint224 total;
+    // BlockNumber for the last update
+    uint32 fromBlock;
+  }
 
-    struct TotalLock {
-        // Total locked Supply
-        uint224 total;
-        // BlockNumber for the last update
-        uint32 fromBlock;
-    }
+  function getUserLock(address user) external view returns (UserLock memory);
+  function getUserPastLock(address user, uint256 blockNumber) external view returns (UserLock memory);
 
-    function getUserLock(address user) external view returns(UserLock memory);
-    function getUserPastLock(address user, uint256 blockNumber) external view returns(UserLock memory);
-
-    function getCurrentTotalLock() external view returns(TotalLock memory);
-    function getPastTotalLock(uint256 blockNumber) external view returns(TotalLock memory);
-  
+  function getCurrentTotalLock() external view returns (TotalLock memory);
+  function getPastTotalLock(uint256 blockNumber) external view returns (TotalLock memory);
 }
