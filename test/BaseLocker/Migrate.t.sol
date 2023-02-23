ecc344735c902655678fa564a6f77e604364f0cd
Alberto Centonze <albicento.ac@gmail.com>
2023-02-23 13:08:15 +0100

    test(BaseLocker): Testing abstracted logic

--- test/BaseLocker/Migrate.t.sol
+++ test/BaseLocker/Migrate.t.sol
@@ -4,17 +4,19 @@ pragma solidity 0.8.16;
 import "./BaseLockerTest.sol";
 
 contract Harvest is BaseLockerTest {
+  address randomReceiver = makeAddr("randomReceiver");
+
   function testWhenPaused() public {
     vm.expectRevert("Pausable: not paused");
     vm.prank(admin);
-    dummyLocker.migrate(makeAddr("randomReceiver"));
+    dummyLocker.migrate(randomReceiver);
   }
 
   function testOnlyOwner() public {
     vm.prank(admin);
     dummyLocker.pause();
     vm.expectRevert("Ownable: caller is not the owner");
-    dummyLocker.migrate(makeAddr("randomReceiver"));
+    dummyLocker.migrate(randomReceiver);
   }
 
   function testZeroAddress() public {
@@ -25,5 +27,36 @@ contract Harvest is BaseLockerTest {
     dummyLocker.migrate(zero);
   }
 
-  // TODO test shutdown
+  function testLockerStillAlive() public {
+    vm.startPrank(admin);
+    dummyLocker.pause();
+    vm.expectRevert(Errors.LockerStillAlive.selector);
+    dummyLocker.migrate(randomReceiver);
+    vm.stopPrank();
+  }
+
+  function testExternalShutdown() public {
+    dummyLocker.activateExternalShutdown();
+    vm.startPrank(admin);
+    dummyLocker.pause();
+    dummyLocker.migrate(randomReceiver);
+    vm.stopPrank();
+  }
+
+  function testInternalShutdown() public {
+    vm.startPrank(admin);
+    dummyLocker.pause();
+    dummyLocker.shutdown();
+    dummyLocker.migrate(randomReceiver);
+    vm.stopPrank();
+  }
+
+  function testInternalAndExternalShutdown() public {
+    dummyLocker.activateExternalShutdown();
+    vm.startPrank(admin);
+    dummyLocker.pause();
+    dummyLocker.shutdown();
+    dummyLocker.migrate(randomReceiver);
+    vm.stopPrank();
+  }
 }
