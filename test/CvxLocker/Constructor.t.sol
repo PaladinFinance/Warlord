e8c33163e135553454d04b4cb2518863be01ac52
Alberto Centonze <albicento.ac@gmail.com>
2023-02-23 19:11:05 +0100

    test(CvxLocker): Removed redundant tests

--- test/CvxLocker/Constructor.t.sol
+++ test/CvxLocker/Constructor.t.sol
@@ -7,30 +7,4 @@ contract Constructor is CvxLockerTest {
   function testDefaultBehavior() public {
     assertEq(registry.delegation(address(locker), "cvx.eth"), delegatee);
   }
-
-  function testZeroAddressController() public {
-    vm.expectRevert(Errors.ZeroAddress.selector);
-    new WarCvxLocker(zero, address(redeemModule), address(minter), delegatee);
-  }
-
-  function testZeroAddressRedeemModule() public {
-    vm.expectRevert(Errors.ZeroAddress.selector);
-    new WarCvxLocker(controller, zero, address(minter), delegatee);
-  }
-
-  function testZeroAddressMinter() public {
-    vm.expectRevert(Errors.ZeroAddress.selector);
-    new WarCvxLocker(controller, address(redeemModule), zero, delegatee);
-  }
-
-  function testZeroAddresses() public {
-    vm.expectRevert(Errors.ZeroAddress.selector);
-    new WarCvxLocker(controller, zero, zero, delegatee);
-    vm.expectRevert(Errors.ZeroAddress.selector);
-    new WarCvxLocker(zero, address(redeemModule), zero, delegatee);
-    vm.expectRevert(Errors.ZeroAddress.selector);
-    new WarCvxLocker(zero, zero, address(minter), delegatee);
-    vm.expectRevert(Errors.ZeroAddress.selector);
-    new WarCvxLocker(zero, zero, zero, delegatee);
-  }
 }
