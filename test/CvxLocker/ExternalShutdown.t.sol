a7169be6de78dfbfafefa57f8b75cbf86eec31df
Alberto Centonze <albicento.ac@gmail.com>
2023-02-23 13:21:04 +0100

    test(CvxLocker): Testing external shutdown

--- test/CvxLocker/ExternalShutdown.t.sol
+++ test/CvxLocker/ExternalShutdown.t.sol
@@ -4,5 +4,15 @@ pragma solidity 0.8.16;
 import "./CvxLockerTest.sol";
 
 contract ExternalShutdown is CvxLockerTest {
-// TODO
+  function testDefaultBehavior() public {
+    vm.prank(vlCvx.owner());
+    vlCvx.shutdown();
+
+    vm.startPrank(admin);
+    locker.pause();
+    // No expired locks means that the check for shutdown passed succesfully
+    vm.expectRevert("no exp locks");
+    locker.migrate(alice);
+    vm.stopPrank();
+  }
 }
