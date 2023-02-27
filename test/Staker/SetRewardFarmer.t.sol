5dcb3adb8f466338b7dc2facf67cf7f793c1da85
Alberto Centonze <albicento.ac@gmail.com>
2023-02-27 13:07:19 +0100

    test(Staker): setRewardFarmer

--- test/Staker/SetRewardFarmer.t.sol
+++ test/Staker/SetRewardFarmer.t.sol
@@ -6,18 +6,55 @@ import "./StakerTest.sol";
 contract SetRewardFarmer is StakerTest {
   function testDefaultBehavior(address rewardToken, address farmer) public {
     vm.assume(rewardToken != zero && farmer != zero);
+
+    vm.expectEmit(true, false, false, true);
+    emit SetRewardFarmer(rewardToken, farmer);
+
     vm.prank(admin);
     staker.setRewardFarmer(rewardToken, farmer);
-    staker.rewardFarmers(rewardToken);
+
+    assertEq(staker.rewardFarmers(rewardToken), farmer, "rewardToken should be associated to the corresponding farmer");
   }
 
-  function testZeroRewardToken() public {
+  function testZeroRewardToken(address farmer) public {
+    vm.assume(farmer != zero);
+
+    vm.expectRevert(Errors.ZeroAddress.selector);
+
+    vm.prank(admin);
+    staker.setRewardFarmer(zero, farmer);
   }
 
-  function testZeroFarmer() public {
+  function testZeroFarmer(address rewardToken) public {
+    vm.assume(rewardToken != zero);
+
+    vm.expectRevert(Errors.ZeroAddress.selector);
+
+    vm.prank(admin);
+    staker.setRewardFarmer(rewardToken, zero);
   }
 
-  function testRewardTokenAlreadySet() public {
+  function testZeroAddresses() public {
+    vm.expectRevert(Errors.ZeroAddress.selector);
 
+    vm.prank(admin);
+    staker.setRewardFarmer(zero, zero);
+  }
+
+  function testRewardTokenAlreadySet(address rewardToken, address initialFarmer, address newFarmer) public {
+    vm.assume(rewardToken != zero && initialFarmer != zero && newFarmer != zero);
+    vm.assume(initialFarmer != newFarmer);
+
+    vm.startPrank(admin);
+    staker.setRewardFarmer(rewardToken, initialFarmer);
+
+    vm.expectRevert(Errors.AlreadySetFarmer.selector);
+    staker.setRewardFarmer(rewardToken, newFarmer);
+    vm.stopPrank();
+  }
+
+  function testOnlyOwner(address rewardToken, address farmer) public {
+    vm.expectRevert("Ownable: caller is not the owner");
+    staker.setRewardFarmer(rewardToken, farmer);
   }
 }
