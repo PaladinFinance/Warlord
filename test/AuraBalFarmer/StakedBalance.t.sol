650f3053221ced8fc3a92b2715bb7b733645f3b5
Alberto Centonze <albicento.ac@gmail.com>
2023-03-08 21:57:14 +0100

    test(*Farmer): Added test placeholder

---
+++ test/AuraBalFarmer/StakedBalance.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.16;
+
+import "./AuraBalFarmerTest.sol";
+
+contract StakedBalance is AuraBalFarmerTest {
+// TODO
+}
---
+++ test/CvxCrvFarmer/StakedBalance.t.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.16;
+
+import "./CvxCrvFarmerTest.sol";
+
+contract StakedBalance is CvxCrvFarmerTest {
+// TODO
+}
