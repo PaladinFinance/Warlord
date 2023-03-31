fafff7c6aa9a53c0eb17093c12b9ae7f9697f349
Alberto Centonze <albicento.ac@gmail.com>
2023-03-31 18:20:29 +0200

    test(Redeemer): Added events for testing

--- test/Redeemer/RedeemerTest.sol
+++ test/Redeemer/RedeemerTest.sol
@@ -7,4 +7,11 @@ contract RedeemerTest is WarlordTest {
   function setUp() public virtual override {
     WarlordTest.setUp();
   }
+
+  event NewRedeemTicket(address indexed token, address indexed user, uint256 amount, uint256 redeemIndex);
+  event Redeemed(address indexed token, address indexed user, address receiver, uint256 indexed ticketNumber);
+  event SetWarLocker(address indexed token, address indexed locker);
+  event RedeemFeeUpdated(uint256 oldRedeemFee, uint256 newRedeemFee);
+  event MintRatioUpdated(address oldMintRatio, address newMintRatio);
+  event FeeReceiverUpdated(address oldFeeReceiver, address newFeeReceiver);
 }
