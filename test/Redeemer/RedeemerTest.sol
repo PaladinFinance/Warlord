04b350d566e0b278a15f9bd65c07fd2f1438814d
Alberto Centonze <albicento.ac@gmail.com>
2023-03-31 17:00:43 +0200

    test(Redeemer): recoverERC20

---
+++ test/Redeemer/RedeemerTest.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: Unlicensed
+pragma solidity 0.8.16;
+
+import "../WarlordTest.sol";
+
+contract RedeemerTest is WarlordTest {
+  function setUp() public virtual override {
+    WarlordTest.setUp();
+  }
+}
