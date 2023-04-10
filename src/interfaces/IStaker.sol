b406503ef82036f6003c7089be87539b7930953e
valentin viger <vigervalentin@gmail.com>
2023-04-10 14:24:24 -0400

    Zap to mint & stake

--- src/interfaces/IStaker.sol
+++ src/interfaces/IStaker.sol
@@ -3,4 +3,6 @@ pragma solidity 0.8.16;
 
 interface IStaker {
   function queueRewards(address rewardToken, uint256 amount) external returns (bool);
+
+  function stake(uint256 amount, address receiver) external returns (uint256);
 }
