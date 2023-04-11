d81ff236d24ca419a87727a64ba30d357902ad2c
Alberto Centonze <albicento.ac@gmail.com>
2023-04-11 13:03:47 +0200

    test: Removed non-priority code from coverage

--- coverage.sh
+++ coverage.sh
@@ -2,7 +2,7 @@
 forge coverage --report lcov
 
 #Remove unwanted files
-lcov --remove lcov.info -o lcov.info 'script/*' 'test/*' 'src/utils/*'
+lcov --remove lcov.info -o lcov.info 'script/*' 'test/*' 'src/utils/*' 'src/Distributor.sol'
 
 #Generate html file
 genhtml lcov.info -o ./coverage
