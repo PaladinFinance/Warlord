d619b1e532c117f9ab4f8c360325d8df6e00d98e
Alberto Centonze <albicento.ac@gmail.com>
2023-04-11 12:03:56 +0200

    test(Zap): Added assertion descriptions

--- test/Zap/Constructor.t.sol
+++ test/Zap/Constructor.t.sol
@@ -5,9 +5,9 @@ import "./ZapTest.sol";
 
 contract Constructor is ZapTest {
   function testDefaultBehavior() public {
-    assertEq(address(zap.warToken()), address(war));
-    assertEq(address(zap.minter()), address(minter));
-    assertEq(address(zap.staker()), address(staker));
+    assertEq(address(zap.warToken()), address(war), "war token is not assigned correctly in constructor");
+    assertEq(address(zap.minter()), address(minter), "war minter is not assigned correctly in constructor");
+    assertEq(address(zap.staker()), address(staker), "war staker is not assigned correctly in constructor");
   }
 
   function testZeroAddressMinter() public {
@@ -26,6 +26,11 @@ contract Constructor is ZapTest {
   }
 
   function testMaxAllowance() public {
-    assertEq(IERC20(address(war)).allowance(address(zap), address(staker)), type(uint256).max);
+    assertEqDecimal(
+      IERC20(address(war)).allowance(address(zap), address(staker)),
+      type(uint256).max,
+      18,
+      "allowance should be set to max"
+    );
   }
 }
