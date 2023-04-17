fadd8acf6e21cdb0d9c22e3f2e3f30dc9d73a15b
Alberto Centonze <albicento.ac@gmail.com>
2023-04-17 10:48:14 +0200

    chore: Added TODO

--- src/Redeemer.sol
+++ src/Redeemer.sol
@@ -9,6 +9,7 @@ pragma solidity 0.8.16;
 //SPDX-License-Identifier: BUSL-1.1
 
 import {Owner} from "utils/Owner.sol";
+// TODO exact imports
 import "openzeppelin/token/ERC20/IERC20.sol";
 import "openzeppelin/token/ERC20/utils/SafeERC20.sol";
 import "openzeppelin/security/Pausable.sol";
