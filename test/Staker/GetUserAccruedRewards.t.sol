20b619ddcaca160a5903d354d798338350196bcd
Alberto Centonze <albicento.ac@gmail.com>
2023-04-18 17:50:15 +0200

    test(Staker): Adding constraints to getters tests

--- test/Staker/GetUserAccruedRewards.t.sol
+++ test/Staker/GetUserAccruedRewards.t.sol
@@ -5,33 +5,30 @@ import "./StakerTest.sol";
 
 contract GetUserAccruedRewards is StakerTest {
   function testDefaultBehavior(uint256 seed) public {
+    vm.assume(seed < 4 weeks);
     // uint256 seed = 1;
     (address[] memory stakers, RewardAndAmount[] memory rewards) = fuzzRewardsAndStakers(seed, 10);
     vm.warp(block.timestamp + seed);
 
-    /*for (uint256 i; i < stakers.length; ++i) {
-      vm.prank(stakers[i]);
-      staker.unstake(type(uint256).max, stakers[i]);
-    } */
-
     address[] memory unstakers = fuzzUnstakers(seed, stakers);
 
     for (uint256 i; i < stakers.length; ++i) {
-      // console.log("new staker --------------");
       for (uint256 j; j < rewards.length; ++j) {
         uint256 accruedAmount = staker.getUserAccruedRewards(rewards[j].reward, stakers[i]);
-        // console.log(accruedAmount);
       }
     }
   }
 
   function fuzzUnstakers(uint256 seed, address[] memory stakers) public returns (address[] memory unstakers) {
-    for (uint256 i; i < stakers.length; ++i) {
+    /*for (uint256 i; i < stakers.length; ++i) {
       console.log(staker.balanceOf(stakers[i]));
-    }
+    }*/
 
     if (stakers.length == 0) return new address[](0);
-    uint256 unstakerIncrease = seed % stakers.length;
+    uint256 unstakerIncrease = seed % stakers.length + 1;
+    /*console.log(seed);
+    console.log(stakers.length);
+    console.log(unstakerIncrease);*/ 
 
     for (uint256 i = unstakerIncrease; i < stakers.length; i += unstakerIncrease) {
       address unstaker = stakers[i];
@@ -40,13 +37,6 @@ contract GetUserAccruedRewards is StakerTest {
       if (unstakerAmount == 0) unstakerAmount = type(uint256).max;
       vm.prank(unstaker);
       staker.unstake(unstakerAmount, stakers[i]);
-      console.log(i);
-    }
-
-    console.log("done");
-
-    for (uint256 i; i < stakers.length; ++i) {
-      console.log(staker.balanceOf(stakers[i]));
     }
   }
 }
