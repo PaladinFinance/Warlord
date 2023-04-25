3287d2ad1c2cb0c16bd27d5aa7f8f715cd80052a
Alberto Centonze <albicento.ac@gmail.com>
2023-04-25 09:39:11 +0200

    Revert "fix: Added roles to token in deployment"
    This reverts commit bf4e8b5f7b220a141450567c7d8ff1f2b163951a.

--- src/AuraLocker.sol
+++ src/AuraLocker.sol
@@ -112,21 +112,4 @@ contract WarAuraLocker is IncentivizedLocker {
     // withdraws rewards to controller
     _harvest();
   }
-
-  /**
-   * @notice Recover ERC2O tokens in the contract
-   * @dev Recover ERC2O tokens in the contract
-   * @param _token Address of the ERC2O token
-   * @return bool: success
-   */
-  function recoverERC20(address _token) external onlyOwner returns (bool) {
-    if (_token == address(aura)) revert Errors.RecoverForbidden();
-
-    if (_token == address(0)) revert Errors.ZeroAddress();
-    uint256 amount = IERC20(_token).balanceOf(address(this));
-    if (amount == 0) revert Errors.ZeroValue();
-    IERC20(_token).safeTransfer(msg.sender, amount);
-
-    return true;
-  }
 }
