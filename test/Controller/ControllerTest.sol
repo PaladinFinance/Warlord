9ec307e66ed5b158c55df87573284d293f4e1839
Alberto Centonze <albicento.ac@gmail.com>
2023-04-30 23:46:26 +0200

    test: Added controller support for claims

--- test/Controller/ControllerTest.sol
+++ test/Controller/ControllerTest.sol
@@ -7,6 +7,7 @@ import {WarStaker} from "src/Staker.sol";
 import {WarRatios} from "src/Ratios.sol";
 import {WarMinter} from "src/Minter.sol";
 import "src/Controller.sol";
+import "src/IncentivizedLocker.sol";
 
 contract ControllerTest is WarlordTest {
   event PullTokens(address indexed swapper, address indexed token, uint256 amount);
@@ -20,13 +21,35 @@ contract ControllerTest is WarlordTest {
   event SetFarmer(address indexed token, address famer);
   event SetDistributionToken(address indexed token, bool distribution);
 
+  IIncentivizedLocker dummyLocker;
+
   function setUp() public virtual override {
     WarlordTest.setUp();
 
-    vm.startPrank(admin);
+    init();
+  }
+
+  function init() public {
+    vm.prank(admin);
     controller =
     new Exposed_Controller(address(war), address(minter), address(staker), swapper, incentivesClaimer, protocolFeeReceiver);
-    vm.stopPrank();
+    dummyLocker =
+      new DummyIncentivizedLocker(address(controller), makeAddr("redeemModule"), address(minter), makeAddr("delegate"));
+  }
+
+  function deployLockerAt(address target) public returns (IncentivizedLocker) {
+    bytes32[] memory targetMemory = new bytes32[](9);
+    // Copy contract storage to stack
+    for (uint256 i; i < targetMemory.length; i++) {
+      targetMemory[i] = vm.load(address(dummyLocker), bytes32(i));
+    }
+    require(target > address(10)); // avoid conflicts with precompiled contracts
+    bytes memory code = address(dummyLocker).code;
+    vm.etch(target, code);
+    for (uint256 i; i < targetMemory.length; i++) {
+      vm.store(target, bytes32(i), targetMemory[i]);
+    }
+    return IncentivizedLocker(target);
   }
 }
 
@@ -51,4 +74,60 @@ contract Exposed_Controller is WarController {
 
 function expose(WarController c) pure returns (Exposed_Controller) {
   return Exposed_Controller(address(c));
+}
+
+contract QuestTest is ControllerTest {
+  function setUp() public virtual override {
+    WarlordTest.setUp();
+
+    // https://etherscan.io/tx/0x69748ad386d6455d1c0c2d1fa0f63ca1c025b0c2787f0911ac032f93250dc52e
+    vm.createSelectFork(vm.rpcUrl("ethereum_alchemy"), 16_852_914);
+
+    init();
+  }
+}
+
+contract DelegationAddressTest is ControllerTest {
+  function setUp() public virtual override {
+    WarlordTest.setUp();
+
+    // https://etherscan.io/tx/0x869e684381f31a6282779a453bbc4e89352f7c4400890d29747108a5ccf5ce70
+    vm.createSelectFork(vm.rpcUrl("ethereum_alchemy"), 17_137_056);
+
+    init();
+  }
+}
+
+contract VotiumTest is ControllerTest {
+  function setUp() public virtual override {
+    WarlordTest.setUp();
+
+    // https://etherscan.io/tx/0xf8c0aa5f030fb808b8536f794036921a0b4bcd3324abe642961fd77820c56ef4
+    vm.createSelectFork(vm.rpcUrl("ethereum_alchemy"), 17_142_838);
+
+    init();
+  }
+}
+
+contract HiddenHandTest is ControllerTest {
+  function setUp() public virtual override {
+    WarlordTest.setUp();
+
+    // https://etherscan.io/tx/0xda4319f4f8f1da3c46a26073191c28017066c13309e38afcb28e7a7f0cf58371
+    vm.createSelectFork(vm.rpcUrl("ethereum_alchemy"), 17_136_627);
+
+    init();
+  }
+}
+
+contract DummyIncentivizedLocker is IncentivizedLocker {
+  constructor(address _controller, address _redeemModule, address _warMinter, address _delegatee)
+    WarBaseLocker(_controller, _redeemModule, _warMinter, _delegatee)
+  {}
+  function _harvest() internal override {}
+  function _lock(uint256 amount) internal override {}
+  function _migrate(address receiver) internal override {}
+  function _processUnlock() internal override {}
+  function _setDelegate(address _delegatee) internal override {}
+  function token() external view returns (address none) {}
 }
