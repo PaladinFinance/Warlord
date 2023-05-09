5c72d8e9b41f7b55c456a473dce33a0f4f206675
Alberto Centonze <albicento.ac@gmail.com>
2023-05-09 14:29:25 +0200

    test(Controller): test `harvestAllAndProcessAll`

--- test/Controller/HarvestAllAndProcessAll.t.sol
+++ test/Controller/HarvestAllAndProcessAll.t.sol
@@ -6,15 +6,15 @@ import "./ControllerTest.sol";
 contract HarvestAllAndProcessAll is UnexposedControllerTest {
   function testDefaultBehavior() public {
     for (uint256 i; i < 2; ++i) {
-      // TODO use exposed controller
       address locker = controller.lockers(i);
       vm.expectCall(locker, abi.encodeCall(IHarvestable(locker).harvest, ()), 1);
+      vm.expectCall(locker, abi.encodeCall(IHarvestable(locker).rewardTokens, ()), 1);
     }
 
     for (uint256 i; i < 2; ++i) {
-      // TODO use exposed controller
       address farmer = controller.farmers(i);
       vm.expectCall(farmer, abi.encodeCall(IHarvestable(farmer).harvest, ()), 1);
+      vm.expectCall(farmer, abi.encodeCall(IHarvestable(farmer).rewardTokens, ()), 1);
     }
     controller.harvestAllAndProcessAll();
   }
