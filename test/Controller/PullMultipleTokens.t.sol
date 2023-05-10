b989a290f0a9c6b551b7a220e7f0e50b9969d201
Alberto Centonze <albicento.ac@gmail.com>
2023-05-10 10:39:10 +0200

    test(Controller): pullMultipleTokens edge cases

---
+++ test/Controller/PullMultipleTokens.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "./ControllerTest.sol";
+
+contract PullMultipleToken is UnexposedControllerTest {
+  function testDefaultBehavior() public {
+    // TODO
+  }
+
+  function testEmptyArray() public {
+    vm.expectRevert(Errors.EmptyArray.selector);
+
+    vm.prank(swapper);
+    controller.pullMultipleTokens(new address[](0));
+  }
+}
+ 
