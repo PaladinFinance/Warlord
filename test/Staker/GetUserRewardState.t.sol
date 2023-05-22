b85e7e1d347195ca31512bebd12a3c5e649dac52
Alberto Centonze <albicento.ac@gmail.com>
2023-05-22 16:17:32 +0200

    test(Staker): Pushing broken test

--- test/Staker/GetUserRewardState.t.sol
+++ test/Staker/GetUserRewardState.t.sol
@@ -4,5 +4,18 @@ pragma solidity 0.8.16;
 import "./StakerTest.sol";
 
 contract GetUserRewardState is StakerTest {
-  function testDefaultBehavior() public {}
+  function testDefaultBehavior() public {
+    (address[] memory stakers, RewardAndAmount[] memory rewards) = fuzzRewardsAndStakers(234908, 4);
+
+    vm.warp(3 days);
+
+    // vm.breakpoint("a");
+
+    vm.prank(stakers[0]);
+    staker.claimRewards(rewards[0].reward, stakers[0]);
+
+    WarStaker.UserRewardState memory state = staker.getUserRewardState(rewards[0].reward, stakers[0]);
+    console.log(state.lastRewardPerToken);
+    console.log(state.accruedRewards);
+  }
 }
