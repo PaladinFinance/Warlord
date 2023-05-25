b36d92fd5cfd97e3c05d28c2f3b92cb235d0b513
Alberto Centonze <albicento.ac@gmail.com>
2023-05-25 22:18:13 +0200

    test(Lockers): Testing multiple locks

--- test/AuraLocker/Lock.t.sol
+++ test/AuraLocker/Lock.t.sol
@@ -11,11 +11,38 @@ contract Lock is AuraLockerTest {
     vm.prank(address(minter));
     locker.lock(amount);
 
-    (, uint256 unlocked, uint256 locked,) = vlAura.lockedBalances(address(locker));
+    (, uint256 unlocked, uint256 locked, AuraLocker.LockedBalance[] memory lockData) =
+      vlAura.lockedBalances(address(locker));
+
     assertEq(locked, amount, "locked balance is equivalent to initial aura amount");
     assertEq(aura.balanceOf(address(minter)), initialAmount - amount, "locked amount is deducted from aura balance");
     assertEq(unlocked, 0, "there should be no unlocked balance");
+    assertEq(lockData[0].amount, amount, "amount should correspond to lockdata amount");
   }
 
-  // TODO more advanced testing with lockData (check in lockedBalances)
+  function testMultipleLocks(uint256[] memory amounts) public {
+    vm.assume(amounts.length < 10);
+    vm.startPrank(address(minter));
+
+    uint256 totalLocked;
+    for (uint256 i; i < amounts.length; ++i) {
+      amounts[i] = amounts[i] % 1e30 + 1;
+      deal(address(aura), address(minter), amounts[i]);
+      locker.lock(amounts[i]);
+      vm.warp(block.timestamp + 7 days);
+      totalLocked += amounts[i];
+    }
+    vm.stopPrank();
+
+    (,, uint256 locked, AuraLocker.LockedBalance[] memory lockData) = vlAura.lockedBalances(address(locker));
+
+    assertEq(locked, totalLocked, "locked amount should correspond to total locked tokens");
+    for (uint256 i; i < amounts.length; ++i) {
+      assertEq(lockData[i].amount, amounts[i], "amounts should correspond to lockdata amount");
+      if (i + 1 == lockData.length) continue;
+      assertLt(
+        lockData[i].unlockTime, lockData[i + 1].unlockTime, "The current lock should be smaller than the previous one"
+      );
+    }
+  }
 }
